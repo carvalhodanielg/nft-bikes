7bb73133ed0f443d2ba9bd99caf5462d26afb9e5
carvalhodaniel <carvalhodanielg@gmail.com>
2022-04-01 13:15:44 -0300

    adding the slider effect to the sections

---
+++ js/scripts.js
@@ -0,0 +1,33 @@
+const target = document.querySelectorAll('[data-anime]')
+const animationClass = 'animate'
+
+
+function animeScroll(){
+    const windowTop = pageYOffset + (.8*window.innerHeight);
+
+    target.forEach(function (element){
+        
+   
+        if(windowTop>element.offsetTop){
+
+            element.classList.add(animationClass)
+        }
+   
+    })
+
+
+    
+
+
+}
+
+
+function scrollAnime(){
+    window.addEventListener('scroll', function (){
+        animeScroll()
+    })
+}
+
+
+scrollAnime()
+
